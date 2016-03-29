d7b45bdf3599a026f50e2b110ac213af2878c934
Katarzyna Starachowicz <kstarachowicz@gmail.com>
2016-03-29 09:25:55 +0200

    Add logic for asking about new game

--- messages.rb
+++ messages.rb
@@ -12,13 +12,22 @@ class Message
   def version
     @socket.gets.split.last
   end
+
+  def body
+    body = @socket.recv(1000).split("\n").last if method == "POST"
+    Hash[JSON.parse(body).map { |key, value| [key.to_sym, value] }] #symbolize keys
+  end
+
+  def ask_new_game?
+    body[:hello] == "play a new game"
+  end
 end
 
 class MyMessage
   def initialize
   end
 
-  def start_play
-    {welcome: "Let's play!"}.to_json
+  def start_play(action)
+    {player_turn: action}.to_json
   end
 end
--- web_server.rb
+++ web_server.rb
@@ -6,7 +6,13 @@ server = TCPServer.new('localhost', 2050)
 loop do
   socket = server.accept
   http = Message.new(socket)
-  response = MyMessage.new.start_play
+    response = if http.ask_new_game?
+                 random_action = rand(2).even? ? "shoot" : "save"
+                 my_message = MyMessage.new
+                 game = Game.new
+                 game.action = random_action
+                 my_message.start_play(random_action)
+               end
   socket.print "#{http.version} 200 OK\r\n" +
                "Content-Type: application/json\r\n" +
                "Content-Length: #{response.bytesize}\r\n" +
